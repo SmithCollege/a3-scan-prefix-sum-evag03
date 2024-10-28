acb65cedf6ca3e6b60682b445384490b84a00def
evag03 <egittardi@smith.edu>
2024-10-28 14:00:19 -0400

    Complete Naive Approach, no progress yet on recursive doubling

--- naiveApproach.cu
+++ naiveApproach.cu
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#define SIZE 100
+
+#define SIZE 128
 #include <sys/time.h>
 
-__global__ void scan(int *input, int *output) {
+__global__ void scan(int* input, int* output) {
   int gindex = threadIdx.x + blockIdx.x * blockDim.x;
-  int lindex = threadIdx.x;
+  // int lindex = threadIdx.x;
 
   //Array size check: thread will do nothing if it's beyond the input array size
-  if (gindex >= sizeof(input)) {
+  if (gindex >= SIZE) {
     return;
   }
   
   // do the scan
-  for (int i = SIZE; i < SIZE; i++) {
+  for (int i = 0; i < SIZE; i++) {
     int value = 0;
     for (int j = 0; j <= i; j++) {
       value += input[j];
@@ -22,43 +23,52 @@ __global__ void scan(int *input, int *output) {
     output[i] = value;
   }
   
-}
+  __syncthreads();
+ }
   
-double get_clock() {
- struct timeval tv; int keroppi;
-  keroppi = gettimeofday(&tv, (void *) 0);
-   if (keroppi<0) { printf("gettimeofday error"); }
+ double get_clock() {
+  struct timeval tv;
+  int ok = gettimeofday(&tv, (void *) 0);
+    if (ok < 0) {
+      printf("gettimeofday error");
+    }
     return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
 }
 
-int i;
-int* times;
-int N;
+
 int main(void) {
+ // int N = 100;
+ // Our SIZE is N (array size)
+
+ int *input, *output;
+ 
  double t0 = get_clock();
+
   // allocate memory
-    int* input = (int*)malloc(sizeof(int) * SIZE);
-    int* output = (int*)malloc(sizeof(int) * SIZE);
+    cudaMallocManaged(&input, SIZE*sizeof(int));
+    cudaMallocManaged(&output, SIZE*sizeof(int));
 
   // initialize inputs
     for (int i = 0; i < SIZE; i++) {
       input[i] = 1 ;
-      }
+    }
 
   // run the kernel
-	scan<<<1,128>>>(input, output);
+    scan<<<1,128>>>(input, output);
 
-  //synchronize
-  cudaDeviceSynchronize();
+  // synchronize
+    cudaDeviceSynchronize();
 
    double t1 = get_clock();
-   printf("time per call: %f\n", t1 - t0);
+   printf("time per call: %f ns\n", t1 - t0);
    
-   // get results
+   // check results
    for (int i = 0; i < SIZE; i++) {
        printf("%d ", output[i]);
    }
    printf("\n");
+
+   printf("%s\n", cudaGetErrorString(cudaGetLastError()));
 
    // free memory
    cudaFree(input);
